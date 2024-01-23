de938e4a8cb760e1a6d4e2b5ba5cbae8b01339f4
IvanIsak2000 <vingving648@gmail.com>
2024-01-23 21:16:39 +0300

    add finane app

---
+++ finance.py
@@ -0,0 +1,64 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+from typing import Optional
+from sqlmodel import SQLModel
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+KEY = os.environ.get('KEY')
+
+
+class CryptoToken(SQLModel):
+    id_: int
+    name: str 
+    slug: str 
+    price_by_USD: float 
+ 
+token_id = 1 #USD
+
+
+#последние котировки
+url ='https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+parameters = {
+    'id':token_id,  
+}
+
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': f'{KEY}'
+}
+
+# все крипетовалюты
+# url = 'https://pro-api.coinmarketcap.com//v1/cryptocurrency/quotes/latest'
+# parameters = {
+#     'start':'1',
+#     'limit':'5'
+    
+# }
+
+
+session = Session()
+session.headers.update(headers)
+response = session.get(url, params=parameters)
+
+if response.status_code == 200:
+    data = json.loads(response.text)
+    status_code = data['status']['error_code']
+
+    if status_code == 0:
+        for _, i in data['data'].items():
+
+            token = CryptoToken(
+                id_=int(i['id']),
+                name=str(i['name']),
+                slug = str(i['slug']),
+                price_by_USD=i['quote']['USD']['price']
+                )
+            print(token)
+
+    else:
+        print(f'{status_code}: {data["status"]["error_message"]}')
+else:
+    print(response.text)
