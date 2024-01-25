1c4f8dfef005fe733766a63be095497ab5a41397
IvanIsak2000 <vingving648@gmail.com>
2024-01-25 23:16:38 +0300

    add new handlers, add currencies button menu

--- bot/bot.py
+++ bot/bot.py
@@ -11,6 +11,16 @@ from typing import Annotated
 from sqlalchemy import String, create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+from aiogram import Router, F
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardRemove,ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
+# from keyboards.simple_row import make_row_keyboard
 
 from config import settings
 
@@ -23,106 +33,73 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=settings.GET_KEYS['BOT_KEY'])
 dp = Dispatcher()
 
-tokens = ['Bitcoin', 'Ethereum', 'Tether USDt', 'BNB', 'Solana', 'XRP', 'USDC', 'Cardano', 'Dogecoin', 'Avalanche', 'TRON', 'Chainlink', 'Polkadot', 'Toncoin', 'Polygon', 'Dai', 'Shiba Inu', 'Litecoin', 'Internet Computer', 'Bitcoin Cash']
+# router = Router()
+
+currencies = ['Bitcoin', 'Ethereum', 'Tether USDt', 'BNB', 'Solana', 'XRP', 'USDC', 'Cardano', 'Dogecoin', 'Avalanche', 'TRON', 'Chainlink', 'Polkadot', 'Toncoin', 'Polygon', 'Dai', 'Shiba Inu', 'Litecoin', 'Internet Computer', 'Bitcoin Cash']
+
+# def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
+#     kb = [
+#         KeyboardButton(text=item) for item in items
+#         ]
+    # return ReplyKeyboardMarkup(keyboard=[kb])
+
+def make_row_keyboard(items):
+    builder = ReplyKeyboardBuilder()
+    for i in items:
+        builder.add(types.KeyboardButton(text=i))
+    builder.adjust(4)
+    return builder.as_markup(resize_keyboard=True)
+
+class TrackingOrder(StatesGroup):
+    choosing_currencies = State()
+    choosing_time = State()
 
 
 @dp.message(Command("start"))
-async def cmd_start(message: types.Message):
+async def start(message: types.Message):
+ 
     await message.answer(
-        '''
+'''
     Hello! 
 I am a modern bot for tracking cryptocurrencies. I can:
 - tracking the cryptocurrencies
 - send you currency data on a schedule
 
-Write /list to select the trackings tokens
-    ''')
-
-@dp.message(Command("list"))
-async def get_currencies_list(message: types.Message):
-    kb = [
-        [types.KeyboardButton(text="Bitcoin")],
-        [types.KeyboardButton(text="Ethereum")],
-        [types.KeyboardButton(text="Tether USDt")],
-        [types.KeyboardButton(text="BNB")],
-        [types.KeyboardButton(text="Solana")],
-        [types.KeyboardButton(text="XRP")],
-        [types.KeyboardButton(text="USDC")],
-        [types.KeyboardButton(text="Cardano")],
-        [types.KeyboardButton(text="Dogecoin")],
-        [types.KeyboardButton(text="Avalanche")],
-        [types.KeyboardButton(text="TRON")],
-        [types.KeyboardButton(text="Chainlink")],
-        [types.KeyboardButton(text="Polkadot")],
-        [types.KeyboardButton(text="Toncoin")],
-        [types.KeyboardButton(text="Polygon")],
-        [types.KeyboardButton(text="Dai")],
-        [types.KeyboardButton(text="Shiba Inu")],
-        [types.KeyboardButton(text="Litecoin")],
-        [types.KeyboardButton(text="Internet Computer")],
-        [types.KeyboardButton(text="Bitcoin Cash")]
-    ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
-
-    await message.answer('Please select token to tracking!', reply_markup=keyboard)
-
-    if message.text in tokens:
-        await message.answer('Greatly! Next write the time:')
-        selected_time = message.text
-        # if selected_time:
-        #     add_to_db()
-
-        # i 
-    
-    else:
-        await message.answer('Sorry, repeat the token select!')
-
-# @dp.message(Command("select"))
-# async def cmd_start(message: types.Message):
-#     kb = [
-#         [
-#             types.KeyboardButton(text="BTC"),
-#             types.KeyboardButton(text="USD")
-#         ],
-#     ]
-#     keyboard = types.ReplyKeyboardMarkup(
-#         keyboard=kb,
-#         resize_keyboard=True,
-#         input_field_placeholder="Выберите способ подачи"
-#     )
-#     await message.answer("Select tokens", reply_markup=keyboard)
+Click Go! button or write it! 
+    ''', reply_markup=make_row_keyboard(['Go!', 'Need help']))
 
 
-#парсит бд, выбирает юзера, парсит токены и отправляет пользователю
-@dp.message(Command("select"))
-async def cmd_start(message: types.Message):
-    
-    Session = sessionmaker(engine)
-    session = Session()
+@dp.message(StateFilter(None), F.text=='Go!')
+async def cmd_currencies(message: types.Message, state: FSMContext):
+    await message.answer(
+        text='Which currencies you want to tracking?', 
+        reply_markup=make_row_keyboard(currencies))
+    await state.set_state(TrackingOrder.choosing_currencies)
+
+
+@dp.message(
+    TrackingOrder.choosing_currencies,
+    F.text.in_(currencies)
+)
+async def currencies_chosen(message: Message, state: FSMContext):
+    await state.update_data(choosen_currencies=message.text)
+    await message.answer(
+        text='Ok,set time:  first hours, after minutes',
+        
+        reply_markup=make_row_keyboard([h for h in range(0,25)], [m for m in range(0,61)])
+    )
+    await state.set_state(TrackingOrder.choosing_time)
+
+
+@dp.message(TrackingOrder.choosing_currencies, F.text.in_(currencies))
+async def final(message: Message, state: FSMContext):
+    data = await state.get_data()
+
+    await message.answer(text=f'{message.text} {data["choosen_currencies"]}')
 
 
 
 
-
-# @dp.message(Command("inline_url"))
-# async def cmd_inline_url(message: types.Message, bot: Bot):
-#     builder = InlineKeyboardBuilder()
-
-#     builder.row(types.InlineKeyboardButton(
-#         text="Bitcoin", url="https://github.com")
-#     )
-#     builder.row(types.InlineKeyboardButton(
-#         text="Ethereum",
-#         url="tg://resolve?domain=telegram")
-#     )
-
-#     # user_id =message.from_user.id
-#     # chat_info = await bot.get_chat(user_id)
-
-#     await message.answer(
-#         'Выберите ссылку',
-#         reply_markup=builder.as_markup(),
-#     )
 # сделать:
 # 1. пользователь выбирает валюты
 # 2. подтверждает выбор
