c888dc307e72cca09ed12d26fec524db72d90b4c
IvanIsak2000 <vingving648@gmail.com>
2024-02-08 23:55:54 +0300

    clear hand move back

--- bot/handlers/new_order.py
+++ bot/handlers/new_order.py
@@ -13,9 +13,14 @@ from sqlalchemy import select
 from kbs.make_keyboard import make_keyboard
 from model import new_order
 from handlers.constants import SUPPORTED_CURRENCIES
-from handlers.constants import DAY_MODE
+from handlers.constants import DAY_MODES
 from handlers.constants import CONSENT
 from handlers.constants import START_BUTTONS
+
+from handlers.constants import WHICH_CURRENCIES_TEXT
+from handlers.constants import WHAT_TIME_TEXT
+from handlers.constants import CONFIRM_OR_NOT_TEXT
+from handlers.constants import ORDER_WAS_CLEAR_TEXT
 
 router = Router()
 
@@ -28,7 +33,7 @@ class OrderTracking(StatesGroup):
 @router.message(StateFilter(None), F.text=='Go')
 async def order_start(message: types.Message, state: FSMContext):
     await message.answer(
-        text='Which currencies you want to tracking?', 
+        text=WHICH_CURRENCIES_TEXT, 
         reply_markup=make_keyboard(SUPPORTED_CURRENCIES))
     await state.set_state(OrderTracking.SUPPORTED_CURRENCIES_choosed)
 
@@ -38,22 +43,22 @@ async def order_start(message: types.Message, state: FSMContext):
     F.text.in_(SUPPORTED_CURRENCIES)
 )
 async def time_choosing(message: Message, state: FSMContext):
-    await state.update_data(choosen_SUPPORTED_CURRENCIES=message.text)
+    await state.update_data(choosen_currency=message.text)
     await message.answer(
-        text='At what time?',
-        reply_markup=make_keyboard(DAY_MODE)
+        text=WHAT_TIME_TEXT,
+        reply_markup=make_keyboard(DAY_MODES)
     )
     await state.set_state(OrderTracking.time_choosed)
 
 
 @router.message(
     OrderTracking.time_choosed,
-    F.text.in_(DAY_MODE)
+    F.text.in_(DAY_MODES)
 )
 async def confirming(message: Message, state: FSMContext):
     await state.update_data(time=message.text)
     await message.answer(
-        text='Do you confirm?',
+        text=CONFIRM_OR_NOT_TEXT,
         reply_markup=make_keyboard(CONSENT)
     )
     await state.set_state(OrderTracking.user_has_confirmed)
@@ -73,7 +78,7 @@ async def confirmation_of_data(message: Message, state: FSMContext):
     data = await state.get_data()
 
     user_order = UserOrder(
-        currency=data['choosen_SUPPORTED_CURRENCIES'],
+        currency=data['choosen_currency'],
         time_=''.join([i for i in data['time'] if i.isdigit() or i.isalpha()]),
         confirm=True if data['confirm'] == 'Confirm' else False
     )
@@ -93,3 +98,13 @@ async def confirmation_of_data(message: Message, state: FSMContext):
         await message.answer(
         text=f"Sorry, your order not created! Please try again!",
         reply_markup=make_keyboard(START_BUTTONS))
+
+
+
+@router.message(Command('clear'))
+async def clear_states(message: types.Message, state: FSMContext):
+    await state.clear() 
+    await message.answer(
+        text=ORDER_WAS_CLEAR_TEXT,
+        reply_markup=make_keyboard(START_BUTTONS))
+    
