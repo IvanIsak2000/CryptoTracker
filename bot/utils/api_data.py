96ae0e5c782d4b1d2b54f1e55007a3c28ee73262
IvanIsak2000 <vingving648@gmail.com>
2024-02-22 22:03:30 +0300

    move to `utils`

---
+++ bot/utils/api_data.py
@@ -0,0 +1,52 @@
+from requests import Request, Session
+import json
+from typing import Optional
+from pydantic import BaseModel
+import os
+
+KEY = os.environ.get('KEY')
+
+class Currency(BaseModel):
+    id_: int
+    name: str 
+    slug: str 
+    price_by_USD: float 
+    
+def all_currencies() -> float:
+    token_id = 1 #USD
+
+    url ='https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    parameters = {
+        'id':token_id,  
+    }
+
+    headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': f'{KEY}'
+    }
+
+
+    session = Session()
+    session.headers.update(headers)
+    response = session.get(url, params=parameters)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        api_status_code = data['status']['error_code']
+
+        if api_status_code == 0:
+            for _, i in data['data'].items():
+                currency = Currency(
+                    id_=i['id'],
+                    name=i['name'],
+                    slug = i['slug'],
+                    price_by_USD=i['quote']['USD']['price']
+                    )
+            return currency
+
+        else:
+            return {api_status_code: data["status"]["error_message"]}
+    else:
+        return {api_status_code: data["status"]["error_message"]}
+    
+print(all_currencies())
---
+++ bot/utils/model.py
@@ -0,0 +1,68 @@
+from sqlalchemy import select, String, BigInteger, delete
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy import create_engine
+from pydantic import BaseModel
+# from data_from_api  
+
+from utils.config import DSN
+
+Base = declarative_base()
+
+
+class OrderTask(Base):
+    __tablename__ = "order_task"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(30))
+    public_name: Mapped[int] = mapped_column(BigInteger)
+    currency: Mapped[int] = mapped_column(String(30))
+    time_is_AM: Mapped[bool]
+    
+    def __repr__(self) -> str:
+        return f"OrderTask(id={self.id!r}, username={self.username!r}, currency={self.currency!r},public_name ={self.public_name!r}, time_is_AM={self.time_is_AM!r})"
+    
+sync_engine = create_engine(DSN)
+
+
+def new_order(username: str, public_name: BigInteger, currency: str, time_is_AM: bool) -> None:
+    with Session(sync_engine) as session:
+        order = OrderTask(username=username,
+                public_name=public_name, 
+                currency=currency,
+                time_is_AM=time_is_AM)
+        session.add(order)
+        session.commit()
+
+
+def get_orders(public_name) -> list[dict]:
+    orders = []
+    with Session(sync_engine) as session:
+        stmt = select(OrderTask).where(OrderTask.public_name==public_name)
+        for i in session.execute(stmt):
+            time_ = '12 AM' if i.OrderTask.time_is_AM else '12 PM '
+            orders.append({'Currency': i.OrderTask.currency, 'Time': time_})
+    return orders
+
+
+def remove_orders(public_name) -> None:
+    with Session(sync_engine) as session:
+        stmt = delete(OrderTask).where(OrderTask.public_name==public_name)
+        session.execute(stmt)
+        session.commit()
+
+
+def get_targets_for_sending() -> list:
+
+    class Target(BaseModel):
+        id: int
+        request_currency: str
+    
+    targets_list = []
+    with Session(sync_engine) as session:
+        stmt = select(OrderTask).where(OrderTask.time_is_AM==True)
+        for i in session.execute(stmt):
+            t = Target(id=i.OrderTask.public_name, request_currency=i.OrderTask.currency)
+            targets_list.append(t)
+    return targets_list
