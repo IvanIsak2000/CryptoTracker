7069a5c6477c7a0bce87a91479dbf47635c7ddcb
IvanIsak2000 <vingving648@gmail.com>
2024-02-26 21:43:29 +0300

    rename all func to `__COMMANDS__`, update text fields

--- bot/handlers/main_handler.py
+++ bot/handlers/main_handler.py
@@ -2,30 +2,37 @@ from aiogram import Router, F
 from aiogram import types
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from kbs.make_keyboard import make_keyboard
-from utils.model import get_orders, remove_orders
+from kbs.make_inline_keyboard import make_inline_keyboard
+from utils.model import get_orders
+from utils.model import remove_one_order
+from utils.model import remove_orders
 
-from handlers.constants import WELCOME_TEXT
 from handlers.constants import START_BUTTONS
 from handlers.constants import MODERATORS
-from handlers.constants import ORDER_WAS_CLEAR_TEXT
+
+from handlers.constants import TEXT_WELCOME
+from handlers.constants import TEXT_ORDER_WAS_CLEAR
+from handlers.constants import TEXT_ONE_ORDER_HAS_BEEN_DELETED
+from handlers.constants import TEXT_ALL_ORDERS_HAS_BEEN_DELETED
 
 router = Router()
 
 
-__all__ = [
+__COMMANDS__ = [
     'start',
     'help',       
     'orders',
     'remove'
+    'clear'
 ]
 
 
 @router.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer(WELCOME_TEXT, reply_markup=make_keyboard(START_BUTTONS))
+    await message.answer(TEXT_WELCOME, 
+    reply_markup=make_keyboard(START_BUTTONS))
 
   
 @router.message(Command('help'))
@@ -36,18 +43,40 @@ async def help(message: types.Message):
 
 @router.message(StateFilter(None), Command('orders'))
 async def get_orders_from_db(message: types.Message, state: FSMContext):
-    await message.answer(text=f'List of your orders:{get_orders(public_name=message.chat.id)}')
+    await message.answer(text=f'List of your orders:', 
+                         reply_markup=make_inline_keyboard(get_orders(public_name=message.chat.id))
+    )
     
+    
+@router.callback_query(lambda d: d.data.startswith('to_remove:'))
+async def remove_order(callback: types.CallbackQuery):
+    currency_to_remove = callback.data.split(":")[1]
+
+    remove_one_order(
+        public_name=callback.message.chat.id,
+        currency=currency_to_remove
+    )
+
+    await callback.answer(
+    text=TEXT_ONE_ORDER_HAS_BEEN_DELETED,
+    show_alert=True,
+    reply_markup=make_inline_keyboard(get_orders(
+        public_name=callback.message.chat.id))
+    )
+
 
 @router.message(StateFilter(None), Command('remove'))
-async def get_orders_from_db(message: types.Message, state: FSMContext):
+async def remove_all_orders(message: types.Message, state: FSMContext):
     remove_orders(public_name=message.chat.id)
-    await message.answer(text='Your orders was removed!',
-        reply_markup=make_keyboard(START_BUTTONS))
+    await message.answer(text=TEXT_ALL_ORDERS_HAS_BEEN_DELETED,
+        reply_markup=make_keyboard(START_BUTTONS)
+    )
     
+
 @router.message(Command('clear'))
 async def clear_states(message: types.Message, state: FSMContext):
     await state.clear() 
     await message.answer(
-        text=ORDER_WAS_CLEAR_TEXT,
-        reply_markup=make_keyboard(START_BUTTONS))
+        text=TEXT_ORDER_WAS_CLEAR,
+        reply_markup=make_keyboard(START_BUTTONS)
+    )
